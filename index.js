8f1a617353bb0a2496047e66d969ccae2d16184b
Sakshi Mishra <sakshi.mishra.21062002@gmail.com>
2026-06-28 08:27:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+async function fetchData(){
+    try{
+        const pokemon  = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+        if (!response.ok){
+            throw new Error("could not fetch dataa ");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite ;
+        imgElement.style.display="block"
+
+    }
+
+    catch(error){
+        console.log(error);
+    }
+}
